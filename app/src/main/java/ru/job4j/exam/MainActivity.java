04b0a59738d9ede26e53166b3d4517d260a831a1
DmK78 <dmk78@inbox.ru>
2019-07-22 11:41:22 +0500

    1. Activity. Жизненный цикл.

--- app/src/main/java/ru/job4j/exam/MainActivity.java
+++ app/src/main/java/ru/job4j/exam/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -16,11 +17,23 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     DatePickerDialog picker;
     EditText eText;
+    private static final String TAG = "ExamActivity";
+    private int rotateCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            rotateCounter = savedInstanceState.getInt("rotateCount");
+
+        }
+        Log.d(TAG, "onCreate");
+        Log.d(TAG, "rotate counter is " + rotateCounter);
+
+
+//данный код предназначен для другой активности, где используется datapicker
         /*eText=(EditText) findViewById(R.id.editText1);
         eText.setInputType(InputType.TYPE_NULL);
         eText.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +58,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("rotateCount", ++rotateCounter);
+    }
 }
