e7cea563062544bba6103c90924b3aac192c820b
DmK78 <dmk78@inbox.ru>
2019-11-14 14:11:53 +0500

    2.1. Рефакторинг. Ссылки на методы. [#130934]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -17,7 +17,7 @@ import ru.job4j.exam.Data.Exam;
 import ru.job4j.exam.Data.Option;
 import ru.job4j.exam.Data.Question;
 
-public class ExamActivity extends AppCompatActivity implements ConfirmHintDialog.ConfirmHintDialogListener{
+public class ExamActivity extends AppCompatActivity implements ConfirmHintDialog.ConfirmHintDialogListener {
 
     public static final String HINT_FOR = "hint_for";
     public static final String QUESTION = "question";
@@ -26,7 +26,7 @@ public class ExamActivity extends AppCompatActivity implements ConfirmHintDialog
     private Button buttonNext;
     private Button buttonHint;
     private Exam currentExam;
-    private Button buttonPrew;
+    private Button buttonPrev;
     private TextView textViewQuestion;
     @Inject
     ExamsCore examsCore;
@@ -38,73 +38,34 @@ public class ExamActivity extends AppCompatActivity implements ConfirmHintDialog
         setContentView(R.layout.activity_main);
         App.getExamCore().injectTo(this);
         Intent intent = getIntent();
-        if(intent!=null){
-            examId=intent.getIntExtra(ExamListFragment.EXAM_ID,-1);
+        if (intent != null) {
+            examId = intent.getIntExtra(ExamListFragment.EXAM_ID, -1);
         }
-        currentExam=examsCore.getExamFromDb(examId);
-        int count=0;
-        for(Question question:currentExam.getQuestions()){
+        currentExam = examsCore.getExamFromDb(examId);
+        int count = 0;
+        for (Question question : currentExam.getQuestions()) {
 
-            if(question.getAnswer()!=-1){
-                position=question.getPosition();
+            if (question.getAnswer() != -1) {
+                position = question.getPosition();
                 count++;
             }
-
         }
-        if(count==currentExam.getQuestions().size()){
-            position=0;
+        if (count == currentExam.getQuestions().size()) {
+            position = 0;
         }
-
         radioGroupVariants = findViewById(R.id.RadioGroupVariants);
         buttonNext = findViewById(R.id.buttonNext);
         buttonHint = findViewById(R.id.buttonHint);
-        buttonPrew = findViewById(R.id.buttonPrew);
+        buttonPrev = findViewById(R.id.buttonPrew);
         textViewQuestion = findViewById(R.id.textViewQuestion);
-        buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int radioButtonID = radioGroupVariants.getCheckedRadioButtonId();
-                if (radioButtonID <0) {
-                    Toast.makeText(getApplicationContext(), "Choose the answer", Toast.LENGTH_SHORT).show();
-                } else {
-                    View radioButton = radioGroupVariants.findViewById(radioButtonID);
-                    int idx = radioGroupVariants.indexOfChild(radioButton);
-                    RadioButton r = (RadioButton)radioGroupVariants.getChildAt(idx);
-                    String s = r.getTag().toString();
-                    int id=Integer.valueOf(s);
-                    currentExam.getQuestions().get(position).setAnswer(id - 1);
-                    examsCore.saveQuestionToDb(currentExam.getId(),currentExam.getQuestions().get(position));
-                    if (position == currentExam.getQuestions().size() - 1) {
-                        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-                        intent.putExtra(ExamListFragment.EXAM_ID,currentExam.getId());
-                        startActivity(intent);
-                    } else {
-                        position++;
-                        fillForm();
-                    }
-                }
-            }
-        });
-        buttonPrew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                position--;
-                fillForm();
-            }
-        });
-        buttonHint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DialogFragment dialog = new ConfirmHintDialog();
-                dialog.show(getSupportFragmentManager(), "dialog_tag");
-            }
-        });
+        buttonNext.setOnClickListener(this::nextButton);
+        buttonPrev.setOnClickListener(this::prevButton);
+        buttonHint.setOnClickListener(this::hintButton);
         this.fillForm();
     }
 
     private void fillForm() {
-        buttonPrew.setEnabled(position != 0);
+        buttonPrev.setEnabled(position != 0);
         radioGroupVariants.clearCheck();
         Question question = currentExam.getQuestions().get(position);
         textViewQuestion.setText(question.getName());
@@ -117,6 +78,39 @@ public class ExamActivity extends AppCompatActivity implements ConfirmHintDialog
                 button.setChecked(true);
             }
         }
+    }
+
+    private void nextButton(View view) {
+        int radioButtonID = radioGroupVariants.getCheckedRadioButtonId();
+        if (radioButtonID < 0) {
+            Toast.makeText(getApplicationContext(), "Choose the answer", Toast.LENGTH_SHORT).show();
+        } else {
+            View radioButton = radioGroupVariants.findViewById(radioButtonID);
+            int idx = radioGroupVariants.indexOfChild(radioButton);
+            RadioButton r = (RadioButton) radioGroupVariants.getChildAt(idx);
+            String s = r.getTag().toString();
+            int id = Integer.valueOf(s);
+            currentExam.getQuestions().get(position).setAnswer(id - 1);
+            examsCore.saveQuestionToDb(currentExam.getId(), currentExam.getQuestions().get(position));
+            if (position == currentExam.getQuestions().size() - 1) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra(ExamListFragment.EXAM_ID, currentExam.getId());
+                startActivity(intent);
+            } else {
+                position++;
+                fillForm();
+            }
+        }
+    }
+
+    private void prevButton(View view) {
+        position--;
+        fillForm();
+    }
+
+    private void hintButton(View view) {
+        DialogFragment dialog = new ConfirmHintDialog();
+        dialog.show(getSupportFragmentManager(), "dialog_tag");
     }
 
     @Override
